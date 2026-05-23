7719918bd1c2f30a6be47a6291b1737caf1ede67
im-lemon <brustenlode@gmail.com>
2026-05-23 13:57:56 +0200

    Add: Reaction when it detects !

--- uwuifier.rb
+++ uwuifier.rb
@@ -1,16 +1,25 @@
 def uwuifier(text)
     if text
-        text = text.gsub('l/i', 'w')
-        text = text.gsub('r/i', "w")
+        text = text.gsub(/l/i, 'w')
+        text = text.gsub(/r/i, "w")
+        text = text.gsub(/na/i, 'nya')
 
     end
     faces = [":3", ">W<", "0.0", "UwU", "OwO", "TwT", "~"]
     face = faces.sample
-    if face == "~"
-        print "#{text}#{face}"
 
+    expressions = ["I am so excitedddd", "Nyaaaaaaaaaaaaa", "UwU UwUuwu./?/.?"]
+    expression = expressions.sample
+    end_text = text
+    if text.include?("!")
+            text = text.gsub('!', '!!!')
+            expression = expressions.sample
+            end_text = "#{text} #{expression}"
+    end
+    if face == "~"
+        print "#{end_text}#{face}"
     else
-        print "#{text} #{face}"
+        print "#{end_text} #{face}"
     end
 end
 
